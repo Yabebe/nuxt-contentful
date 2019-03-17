6b882e0f7197b5aad97f73fef8054b6d1d74d1ad
yabebe <yayube1207@gmail.com>
2019-03-17 20:30:22 +0900

    Connect with Contentful

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 const pkg = require('./package')
+const config = require('./.contentful.json')
+
+
 
 
 module.exports = {
@@ -52,5 +55,13 @@ module.exports = {
     extend(config, ctx) {
       
     }
+  },
+
+
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
   }
 }
